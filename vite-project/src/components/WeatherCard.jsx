fd2cf755c366e615b0d6ddeeb1c6992e5c7da780
Godis Nanu <godisnanu@gmail.com>
2024-06-05 12:40:12 -0400

    adding WeatherCard

--- vite-project/src/components/WeatherCard.jsx
+++ vite-project/src/components/WeatherCard.jsx
@@ -1,5 +1,16 @@
+import "../blocks/weather-card.css";
+
 function WeatherCard() {
-  return <></>;
+  return (
+    <section className="weather-card">
+      <p className="weather-card__temp">current Temp</p>
+      <img
+        src="../src/assets/Cloudy.png"
+        alt="Cloudy"
+        className="weather-card__image"
+      />
+    </section>
+  );
 }
 
 export default WeatherCard;
