2acd336d37bdbb6e0450a8603bb20742898b785e
Godis Nanu <godisnanu@gmail.com>
2024-06-09 12:41:29 -0400

    adding ItemCard

--- vite-project/src/components/App.jsx
+++ vite-project/src/components/App.jsx
@@ -1,13 +1,16 @@
+import { useState } from "react";
 import "../blocks/page.css";
 import Header from "./Header";
 import Main from "./Main";
 
 function App() {
+  const { weatherData, setWeatherData } = useState({ type: "" });
+
   return (
     <div className="page">
       <div className="page__content">
         <Header />
-        <Main />
+        <Main weatherData={weatherData} />
       </div>
     </div>
   );
--- vite-project/src/components/Main.jsx
+++ vite-project/src/components/Main.jsx
@@ -1,6 +1,7 @@
-import WeatherCard from "./WeatherCard";
-
-function Main() {
+import WeatherCard from "./WeatherCard.jsx";
+import { defaultClothingItems } from "../utils/constants.js";
+import ItemCard from "./ItemCard.jsx";
+function Main({ weatherData }) {
   return (
     <main className="main">
       <WeatherCard />
@@ -8,7 +9,11 @@ function Main() {
         <p className="cards__text">
           Today is {/* Temp*/}/ You may want to wear:
         </p>
-        {/* Add Cards */}
+        <ul className="card__list">
+          {defaultClothingItems.map((item) => {
+            return <ItemCard key={item._id} item={item} />;
+          })}
+        </ul>
       </section>
     </main>
   );
