8ff6eaa4581b8d1ac277f84260fb71c3158c0025
Godis Nanu <godisnanu@gmail.com>
2024-06-28 12:06:44 -0400

    ModalForm refactoring

--- vite-project/src/components/App.jsx
+++ vite-project/src/components/App.jsx
@@ -32,7 +32,46 @@ function App() {
       <div className="page__content">
         <Header weatherData={weatherData} />
         <Main weatherData={weatherData} />
-        <ModalWithForm />
+        <ModalWithForm title="New garment" buttonText="Add garment">
+          <label htmlFor="name" className="modal__label">
+            Name{" "}
+            <input
+              type="
+        text"
+              className="modal__input"
+              id="name"
+              placeholder="Name"
+            />
+          </label>
+          <label htmlFor="imageUrl" className="modal__label">
+            Image{" "}
+            <input
+              type="
+        text"
+              className="modal__input"
+              id="imageUrl"
+              placeholder="Image URL"
+            />
+          </label>
+          <fieldset className="modal__radio-buttons">
+            <legend className="modal__legend"> Slect the weather type: </legend>
+            <label htmlFor="hot" className="modal__label modal__radio">
+              <input id="hot" type="radio" className="modal__radio-input" /> Hot
+            </label>
+            <label htmlFor="warm" className="modal__label modal__radio">
+              <input id="warm" type="radio" className="modal__radio-input" />{" "}
+              Warm
+            </label>
+            <label htmlFor="chilly" className="modal__label modal__radio">
+              <input id="chilly" type="radio" className="modal__radio-input" />
+              Chilly
+            </label>
+            <label htmlFor="cold" className="modal__label modal__radio">
+              <input id="cold" type="radio" className="modal__radio-input" />{" "}
+              Cold
+            </label>
+          </fieldset>
+        </ModalWithForm>
       </div>
     </div>
   );
--- vite-project/src/components/ModalWithForm.jsx
+++ vite-project/src/components/ModalWithForm.jsx
@@ -1,56 +1,20 @@
+import { Children } from "react";
 import "../blocks/modal.css";
 
-function ModalWithForm() {
+function ModalWithForm({ children, title, buttonText }) {
   return (
     <div className="modal">
       <div className="modal__content">
-        <h2 className="modal__title">New Garment</h2>
+        <h2 className="modal__title">{title}</h2>
         <button src="" className="modal__close">
           Close
         </button>
         <form action="" className="modal__form">
-          <label htmlFor="name" className="modal__label">
-            Name{" "}
-            <input
-              type="
-        text"
-              className="modal__input"
-              id="name"
-              placeholder="Name"
-            />
-          </label>
-          <label htmlFor="imageUrl" className="modal__label">
-            Image{" "}
-            <input
-              type="
-        text"
-              className="modal__input"
-              id="imageUrl"
-              placeholder="Image URL"
-            />
-          </label>
-          <fieldset className="modal__radio-buttons">
-            <legend className="modal__legend"> Slect the weather type: </legend>
-            <label htmlFor="hot" className="modal__label modal__radio">
-              <input id="hot" type="radio" className="modal__radio-input" /> Hot
-            </label>
-            <label htmlFor="warm" className="modal__label modal__radio">
-              <input id="warm" type="radio" className="modal__radio-input" />{" "}
-              Warm
-            </label>
-            <label htmlFor="chilly" className="modal__label modal__radio">
-              <input id="chilly" type="radio" className="modal__radio-input" />
-              Chilly
-            </label>
-            <label htmlFor="cold" className="modal__label modal__radio">
-              <input id="cold" type="radio" className="modal__radio-input" />{" "}
-              Cold
-            </label>
-          </fieldset>
+          {children}
         </form>
         <button type="submit" className="modal__submit">
           {" "}
-          Add garment{" "}
+          {buttonText}{" "}
         </button>
       </div>
     </div>
